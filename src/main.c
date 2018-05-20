49bf9b52b8f6819f8a9a398d508732898b57283a
Oliver Strong <ostro2@eq.edu.au>
2018-05-20 14:13:03 +1000

    Automated push

--- src/main.c
+++ src/main.c
@@ -6,38 +6,32 @@ void main()
 	//system_init();
 	uart_init();
 	lfb_init();
+	
 	gpio_setup(3, GPIO_OUTPUT);
+	
 	while(1)
 	{
-	int k = 0;
-	for(int pos = 0; pos <= 180; pos++)
-	{
-		k = 13.333333333333 * pos;
-		gpio_output(3, true);
-		wait_usec(k); //Goto 0 degrees if 180 is desired use 2400us
-		gpio_output(3, false);
-		wait_usec(15000 - k);
-		//wait_usec(15000);
+		int k = 0;
+		for(int pos = 0; pos <= 180; pos++)
+		{
+			k = 13.333333333333 * pos;
+			gpio_output(3, true);
+			wait_usec(k); //Goto 0 degrees if 180 is desired use 2400us
+			gpio_output(3, false);
+			wait_usec(15000 - k);
+			//wait_usec(15000);
+		}
+		int i = 0;
+		for(int pos = 180; pos >= 0; pos --)
+		{
+			i = 13.3333333333333 * pos;
+			gpio_output(3, true);
+			wait_usec(i); //Goto 0 degrees if 180 is desired use 2400us
+			gpio_output(3, false);
+			wait_usec(15000 - i);
+			//wait_usec(15000);
+		}
 	}
-	int i = 0;
-	for(int pos = 180; pos >= 0; pos --)
-	{
-		i = 13.3333333333333 * pos;
-		gpio_output(3, true);
-		wait_usec(i); //Goto 0 degrees if 180 is desired use 2400us
-		gpio_output(3, false);
-		wait_usec(15000 - i);
-		//wait_usec(15000);
-	}
-	}
-
-	/*while(1)
-	{
-		gpio_output(3, true);
-		wait_usec(540);
-		gpio_output(3, false);
-		wait_usec(19460);
-	}*/
 
 	char r, g, b;
 	for(int x = 0; x < 1024; x++)
