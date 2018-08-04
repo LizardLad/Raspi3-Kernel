67ff6796ed4585db589ba35a560af296b31eb5fd
Oliver Strong <oliver.russell.strong@gmail.com>
2018-08-04 20:36:12 +1000

    Main cleanup and added the multiline comment to a text file to suppress the error

--- src/main.c
+++ src/main.c
@@ -5,29 +5,11 @@ void main()
 	// set up serial console
 	uart_init();
 	lfb_init();
-	//clocks_init();
+	clocks_init();
 	dynamic_memory_alloc_init();
 	console_init();
 
-	//printf("Hello World! %3x %s", malloc(12 * sizeof(char)), "Well hello there lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll");
-	asm volatile("brk #0");//breakpoint;	
-
-	//init_audio_jack();
-	//play_16bit_unsigned_audio((char *)&_binary_src_audio_The_Amazons_bin_start, (char *)&_binary_src_audio_The_Amazons_bin_end);
-
-	//if(sd_init() == SD_OK)
-	//{
-	//	if(fat_getpartition())
-	//	{
-	//		text_file_dump("LICENC~1BRO");
-	//		fat_listdirectory();
-	//	}
-	//}
-
-	//asm(	"mov	x1, #0xe0\n"\
-	//	"mov	x2, #0x80000\n" \
-	//	"str	x2, [x1]\n"\
-	//	"sev");
+	breakpoint;	
 
 	// echo everything back
 	while(1) 
